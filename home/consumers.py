9136b2d2eabe392644a5c0fa42768d0813f37fa5
Yashvin Goyal <100990409+YashvinGoyal@users.noreply.github.com>
2023-07-08 10:01:41 +0530

    Update consumers.py

--- home/consumers.py
+++ home/consumers.py
@@ -5,12 +5,10 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-# consumers have method to connect a chatbox from client to server, process messages like saving the, and other methods
-
 
 class Chatconsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("connect method running")
+    
         self.chat_box_name = self.scope["url_route"]["kwargs"]["chat_box_name"]
         self.group_name = "chat_%s" % self.chat_box_name
         await self.channel_layer.group_add(self.group_name, self.channel_name)
@@ -19,9 +17,8 @@ class Chatconsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    # This function received messages from WebSocket.
     async def receive(self, text_data):
-        print("Receive method running")
+        
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json["username"]
@@ -35,12 +32,9 @@ class Chatconsumer(AsyncWebsocketConsumer):
             },
         )
 
-    # Receive message from room group.
     async def chatbox_message(self, event):
-        print("chatbox_message method running")
         message = event["message"]
         username = event["username"]
-        # send message and username of sender to websocket
         await self.send(
             text_data=json.dumps(
                 {
