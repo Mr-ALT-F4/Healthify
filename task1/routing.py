8cb7ae708fa4bf418f8534ca62361d7e31acb5dd
Yashvin Goyal <100990409+YashvinGoyal@users.noreply.github.com>
2023-07-08 10:02:56 +0530

    Update routing.py

--- task1/routing.py
+++ task1/routing.py
@@ -3,5 +3,5 @@ from home import consumers
 
 # URLs that handle the WebSocket connection are placed here.
 ws_pattern = [
-    re_path(r'ws/chat/(?P<chat_box_name>\w+)/$', consumers.Chatconsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_code>\w+)/$', consumers.Chatconsumer.as_asgi()),
 ]
